c19f6d81c96dd4586d1adefae569bab2708b64d5
Viet Nguyen <vietnguyen@Viets-MacBook-Air.local>
2023-08-02 17:00:33 -0400

    cleaned up main page to start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,53 +2,25 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from './pages/about';
 import Welcome from './components/Welcome/Welcome';
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
+  // const [count, setCount] = useState<number>(0);
 
   return (
     <Router>
       <div className={styles.App}>
         <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
           <Welcome />
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className={styles['App-link']}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className={styles['App-link']}
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-          <Switch>
-            <Route path="/about">
-              <main>About</main>
-            </Route>
-            <Route path="/">
-              <main>Home</main>
-            </Route>
-          </Switch>
+          <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+          </div>
         </header>
+        <Route path="/about">
+          <About />
+        </Route>
       </div>
     </Router>
   );
---
+++ src/app/pages/about.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function About(): JSX.Element {
+  return <div>this is the about page</div>;
+}
