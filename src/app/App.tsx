710e8dcd1a9963003d1e848b9d6c737cbaaeb6f0
Viet Nguyen <vietnguyen@Viets-MacBook-Air.local>
2023-08-02 17:01:17 -0400

    cleaned up main page to start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import styles from './App.module.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import About from './pages/about';
 import Welcome from './components/Welcome/Welcome';
 
 function App(): JSX.Element {
-  // const [count, setCount] = useState<number>(0);
-
   return (
     <Router>
       <div className={styles.App}>
